e0ed327dfffa3f8ebd472c5689e0a8581c9069a6
varunvp <varunvp7@gmail.com>
2018-07-16 19:17:31 +0530

    Added while loop to MPC Obstacle Avoidance

--- qp_planner/src/MPC_obstacle_avoidance.py
+++ qp_planner/src/MPC_obstacle_avoidance.py
@@ -26,80 +26,81 @@ def MPC_solver(actual=0., desired=0., limit=1000, origin=0, nsteps=10.,interval=
 	Returns:
 		float -- Solution
 	"""
-	delta = 0.5
-	global prev_nsteps, prev_interval, big_I, big_0, dyn_A, dyn_b, term_A, term_b, pos_constraint, vel_constraint, big_H, big_h, big_A_eq, big_Ba_ineq
-	if variables:
-		big_A_eq = variables.get("big_A_eq")
-		big_Ba_ineq = variables.get("big_Ba_ineq")
-		big_H = variables.get("big_H")
-		big_h = variables.get("big_h")
-		prev_nsteps = variables.get("prev_nsteps")
-		prev_interval = variables.get("prev_interval")
+	while True:
+		delta = 0.5
+		global prev_nsteps, prev_interval, big_I, big_0, dyn_A, dyn_b, term_A, term_b, pos_constraint, vel_constraint, big_H, big_h, big_A_eq, big_Ba_ineq
+		if variables:
+			big_A_eq = variables.get("big_A_eq")
+			big_Ba_ineq = variables.get("big_Ba_ineq")
+			big_H = variables.get("big_H")
+			big_h = variables.get("big_h")
+			prev_nsteps = variables.get("prev_nsteps")
+			prev_interval = variables.get("prev_interval")
 
 
-	timer = time.time()
+		timer = time.time()
 
-	if nsteps != prev_nsteps:
-		big_I = np.eye(2*nsteps)			#G
-		big_0 = np.zeros(2*nsteps)
-	
-	#Get dynamic & terminal constraints
-	if (nsteps != prev_nsteps or interval != prev_interval):
-		# [1 -1  0 t 0 0]
-		# [0  1 -1 0 t 0] 
-		# [0  0  1 0 0 t]
-		dyn_A = np.column_stack((np.eye(nsteps , dtype=float) + np.eye(nsteps, nsteps , 1, dtype=float) * -1, np.eye(nsteps, dtype=float) * interval)) 	#C
-		dyn_b = np.zeros(nsteps, dtype=float)
-		term_A = np.zeros((1,2*nsteps), dtype=float)
+		if nsteps != prev_nsteps:
+			big_I = np.eye(2*nsteps)			#G
+			big_0 = np.zeros(2*nsteps)
+		
+		#Get dynamic & terminal constraints
+		if (nsteps != prev_nsteps or interval != prev_interval):
+			# [1 -1  0 t 0 0]
+			# [0  1 -1 0 t 0] 
+			# [0  0  1 0 0 t]
+			dyn_A = np.column_stack((np.eye(nsteps , dtype=float) + np.eye(nsteps, nsteps , 1, dtype=float) * -1, np.eye(nsteps, dtype=float) * interval)) 	#C
+			dyn_b = np.zeros(nsteps, dtype=float)
+			term_A = np.zeros((1,2*nsteps), dtype=float)
 
-		#Concatenate dynamic and terminal LHS constraint
-		big_A_eq = np.row_stack((dyn_A, term_A))
-
-
-	term_b = np.array([actual-desired])
-	
-	#Concatenate dynamic and terminal RHS constraint
-	big_b_eq = np.concatenate((dyn_b, term_b))
-
-	#Inequality constraints(Boundary constraints)
-	if nsteps != prev_nsteps:
-		positive_pos_constraint = np.eye(nsteps, nsteps) * -delta + np.eye(nsteps, nsteps, 1)
-		negative_pos_constraint = np.eye(nsteps, nsteps) * delta - np.eye(nsteps, nsteps, 1)
-		pos_constraint = np.row_stack( (positive_pos_constraint, negative_pos_constraint) )
-		vel_constraint = np.zeros((2 * nsteps, nsteps))
-		big_Ba_ineq = np.column_stack((pos_constraint, vel_constraint))
-
-	#######################################################################################
-	solution = variables.get("solution")				#This is the solution from the previous calculation
-	#Check 'solution' for None
-	#Chaitanya's code here. Put variables that won't change with nstep's or time interval's change inside the above if, for caching
-	####################################################################################### 
-
-	# big_Bb_ineq = np.concatenate((np.ones(nsteps) * (limit + origin - desired), np.ones(nsteps) * (limit - origin + desired)))
-	big_Bb_ineq = np.concatenate((np.ones(nsteps) * ((origin + limit - desired) * (1 - delta)), np.ones(nsteps) * (-(origin - limit - desired) *  (1 - delta))))
-
-	#Relaxation
-	if (nsteps != prev_nsteps or interval != prev_interval):
-		big_H = block_diag([1000], big_I)
-		big_h = np.concatenate(([0], big_0))
-		big_A_eq = np.column_stack((np.zeros(nsteps + 1), big_A_eq))
-		big_A_eq[nsteps-1][0] = -1
-		big_A_eq[nsteps][1] = 1
-		big_Ba_ineq = np.column_stack((np.transpose(np.zeros(2*nsteps)), big_Ba_ineq))
-		big_Ba_ineq[nsteps-1][0] = 1
-		big_Ba_ineq[2 * nsteps-1][0] = -1
+			#Concatenate dynamic and terminal LHS constraint
+			big_A_eq = np.row_stack((dyn_A, term_A))
 
 
-	u_in = qp_matrix.quadprog_solve_qp(big_H, big_h, big_Ba_ineq, big_Bb_ineq, big_A_eq, big_b_eq)
+		term_b = np.array([actual-desired])
+		
+		#Concatenate dynamic and terminal RHS constraint
+		big_b_eq = np.concatenate((dyn_b, term_b))
 
-	if (nsteps != prev_nsteps or interval != prev_interval):
-		variables = {"big_A_eq": big_A_eq, "big_Ba_ineq": big_Ba_ineq, "big_H": big_H, "big_h": big_h, "prev_nsteps": prev_nsteps, "prev_interval": prev_interval, "solution": u_in}
+		#Inequality constraints(Boundary constraints)
+		if nsteps != prev_nsteps:
+			positive_pos_constraint = np.eye(nsteps, nsteps) * -delta + np.eye(nsteps, nsteps, 1)
+			negative_pos_constraint = np.eye(nsteps, nsteps) * delta - np.eye(nsteps, nsteps, 1)
+			pos_constraint = np.row_stack( (positive_pos_constraint, negative_pos_constraint) )
+			vel_constraint = np.zeros((2 * nsteps, nsteps))
+			big_Ba_ineq = np.column_stack((pos_constraint, vel_constraint))
 
-		prev_nsteps = nsteps
-		prev_interval = interval
+		#######################################################################################
+		solution = variables.get("solution")				#This is the solution from the previous calculation
+		#Check 'solution' for None
+		#Chaitanya's code here. Put variables that won't change with nstep's or time interval's change inside the above if, for caching
+		####################################################################################### 
 
-	# print(u_in)
-	# print(time.time() - timer)
+		# big_Bb_ineq = np.concatenate((np.ones(nsteps) * (limit + origin - desired), np.ones(nsteps) * (limit - origin + desired)))
+		big_Bb_ineq = np.concatenate((np.ones(nsteps) * ((origin + limit - desired) * (1 - delta)), np.ones(nsteps) * (-(origin - limit - desired) *  (1 - delta))))
+
+		#Relaxation
+		if (nsteps != prev_nsteps or interval != prev_interval):
+			big_H = block_diag([1000], big_I)
+			big_h = np.concatenate(([0], big_0))
+			big_A_eq = np.column_stack((np.zeros(nsteps + 1), big_A_eq))
+			big_A_eq[nsteps-1][0] = -1
+			big_A_eq[nsteps][1] = 1
+			big_Ba_ineq = np.column_stack((np.transpose(np.zeros(2*nsteps)), big_Ba_ineq))
+			big_Ba_ineq[nsteps-1][0] = 1
+			big_Ba_ineq[2 * nsteps-1][0] = -1
+
+
+		u_in = qp_matrix.quadprog_solve_qp(big_H, big_h, big_Ba_ineq, big_Bb_ineq, big_A_eq, big_b_eq)
+
+		if (nsteps != prev_nsteps or interval != prev_interval):
+			variables = {"big_A_eq": big_A_eq, "big_Ba_ineq": big_Ba_ineq, "big_H": big_H, "big_h": big_h, "prev_nsteps": prev_nsteps, "prev_interval": prev_interval, "solution": u_in}
+
+			prev_nsteps = nsteps
+			prev_interval = interval
+
+		# print(u_in)
+		# print(time.time() - timer)
 
 	return u_in[nsteps+1], variables
 
